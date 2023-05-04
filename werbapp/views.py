e4ca31442cae760693a6f7b13ab055aafadbe003
marioalexandreantunes <mario@marioantunes.pt>
2023-05-04 19:09:38 +0100

    configurações para render.com

--- werbapp/views.py
+++ werbapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# ADD --MA-- irei usar templates para carregar css e js , verificação se estará whitenoise a funcionar
+def index(request):
+    context={}
+    return render(request, 'index.html', context)
